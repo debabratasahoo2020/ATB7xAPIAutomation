819761c8533998bc4c6e71f1cce303703dcdf96d
Debabrata.Sahoo <debatherock@gmail.com>
2024-09-16 00:10:16 +0530

    2 files added

--- src/test/java/Test001.java
+++ src/test/java/Test001.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Test001 {
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
